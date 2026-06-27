5c3f9c4254eba76c00cbd1bec61e8c03875fb432
jacbene <jacquesbene301@gmail.com>
2026-06-27 14:53:15 +0100

    mise a jour de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/pasma-sys/',
-  server: {
-    port: 3000,
-  },
-=======
   base: '/pasma-sys/',  // ← Important : nom de votre dépôt
   server: {
     host: true,
     port: 3000
   }
->>>>>>> 9a1a326 (Fix: Restauration de main.jsx et mise à jour de vite.config.js)
 });
